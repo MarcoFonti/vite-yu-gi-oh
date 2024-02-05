24dc93ffd6bea4740bf8ac6c417fb0c384efd0d5
MarcoFonti <theemporiumg@gmail.com>
2024-02-05 17:44:10 +0100

    add app-main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,11 +3,18 @@
 
 // IMPORTO POKEDEX
 import Pokédex from './Pokédex.vue'
+// IMPORTO STORE
+import { store } from '../data/store';
 
 export default {
     // NOME
     name: 'AppMain',
-    components: { Pokédex }
+    // COMPONENTI
+    components: { Pokédex },
+    // DATI
+    data:() => ({
+        store
+    }),
     
 };
 
@@ -15,11 +22,56 @@ export default {
 
 <!-- HTML -->
 <template>
-    <!-- POKEDEX -->
-    <Pokédex/>
+    <!-- MAIN -->
+    <main>
+        <!-- LOADING -->
+        <h1 v-if="store.loading">Loading...</h1>
+        <!-- SEZIONE POKEDEX -->
+        <section v-else id="recipe-pokedex">
+            <!-- TITOLO -->
+            <h2>pokedex</h2>
+            <!-- RECIPIENTE POKEMONS -->
+            <div class="recipe-pokemons">
+                <!-- POKEDEX -->
+                <Pokédex/>
+            </div>
+        </section>
+    </main>
 </template>
 
 <!-- CSS -->
-<style>
+<style lang="scss" scoped>
+
+/* LOADING  */
+h1 {
+        text-align: center;
+}
+
+/* RECIPIENTE POKEDEX */
+#recipe-pokedex {
+    background-color: rgba(215, 16, 16, 0.532);
+    min-height: 500px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    /* TITOLO */
+    h2 {
+        text-transform: uppercase;
+        text-align: center;
+        font-size: 30px;
+
+    }
+
+}
+
+/* RECIPIENTE POKEMONS */
+.recipe-pokemons {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    border: 1px solid;
+    margin: 10px;
+}
 
 </style>
--- src/components/Pokédex.vue
+++ src/components/Pokédex.vue
@@ -18,10 +18,41 @@ export default {
 
 <!-- HTML -->
 <template>
-
+    <!-- V-FOR PER CREARE LE CARD -->
+    <div class="card" v-for="pokemon in store.pokemons">
+        <img :src="pokemon.imageUrl" :alt="pokemon.name">
+        <div class="recipe-card">
+            <div>{{ pokemon.name }}</div>
+            <div>{{ pokemon.type1 }}</div>
+            <div>{{ pokemon.total}}</div>
+        </div>
+    </div>
 </template>
 
 <!-- CSS -->
-<style>
+<style lang="scss" scoped>
+
+/* CARD */
+.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: calc(100% / 5);
+    text-align: center;
+    background-color: rgb(207, 215, 215);
+    padding-top: 10px;
+    padding-bottom: 10px;
+
+    /* IMMAGINI */
+    img {
+        width: 250px;
+        height: 250px;
+        border-radius: 20%;
+        margin-bottom: 5px ;
+        border: 3px solid;
+    }
+
+}
 
 </style>
